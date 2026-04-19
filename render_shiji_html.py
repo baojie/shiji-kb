7f2364ce4c5e19d7facc632bddac20ed20d86119
baojie <baojie@gmail.com>
2026-04-20 03:08:55 +0800

    修复修辞标注渲染问题
    render_shiji_html.py:
    - 修复单引号替换 raw string 中 \' 输出字面反斜杠的 bug
    
    docs/css/shiji-styles.css:
    - .idiom 改用 text-decoration dotted，替代 background-image tile 对齐方案
      原方案红点无法与汉字逐一对齐
    
    docs/chapters/130_太史公自序.html:
    - 重新生成，消除 \' 符号

--- render_shiji_html.py
+++ render_shiji_html.py
@@ -117,7 +117,7 @@ ENTITY_PATTERNS = [
 QUOTE_PATTERNS = [
     (r'[\u201c]([^\u201d<>]+)[\u201d]', r'<span class="quoted">"\1"</span>'),      # 中文双引号 " "
     (r'(?<!class=)[\u0022]([^\u0022<>]+)[\u0022]', r'<span class="quoted">"\1"</span>'),      # ASCII双引号 " " (排除HTML属性)
-    (r'[\u2018]([^\u2019<>]+)[\u2019]', r'<span class="quoted">\'\1\'</span>'),    # 中文单引号 ' '
+    (r'[\u2018]([^\u2019<>]+)[\u2019]', "<span class=\"quoted\">'\\1'</span>"),    # 中文单引号 ' '
     # ASCII单引号：不处理，因为容易与嵌套引号冲突
     # (r'[\u0027]([^\u0027<>]+)[\u0027]', r'<span class="quoted">\'\1\'</span>'),    # ASCII单引号 ' '
     (r'「([^」<>]+)」', r'<span class="quoted">「\1」</span>'),    # 日式单引号
